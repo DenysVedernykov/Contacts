217f3e73e2998d9feccbd61420db7cb22c850283
Denys Vedernykov <razom.info@gmail.com>
2021-10-10 18:21:27 +0300

    added model Contact

---
+++ Contacts/Contacts/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contacts.Models
+{
+    class Contact
+    {
+        public int Id { get; set; }
+        public int Autor { get; set; }
+        public string Nick { get; set; }
+        public string FullName { get; set; }
+        public string Description { get; set; }
+        public string Number { get; set; }
+        public string PathImage { get; set; }
+        public DateTime TimeCreating { get; set; }
+    }
+}
