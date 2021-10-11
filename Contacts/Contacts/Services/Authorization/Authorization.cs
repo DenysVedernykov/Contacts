6c1ad7d8c35a40e1b4a9b0956c321ba6de116363
Denys Vedernykov <razom.info@gmail.com>
2021-10-11 21:52:12 +0300

    added validation functions to service Authorization

--- Contacts/Contacts/Services/Authorization/Authorization.cs
+++ Contacts/Contacts/Services/Authorization/Authorization.cs
@@ -3,6 +3,7 @@ using Contacts.Services.Repository;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Contacts.Services.Authorization
@@ -20,6 +21,61 @@ namespace Contacts.Services.Authorization
 
         public bool Status { get => _status; }
         public User Profile { get =>_profile; }
+
+        public bool CheckForUse(string login)
+        {
+            bool result = false;
+
+            Task<User> user = _repository.SearchUserByLoginAsync<User>(login);
+            if (user.Result == null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool loginMatching(string login)
+        {
+            bool result = true;
+
+            if (login.Length < 4 || login.Length > 16)
+            {
+                result = false;
+            }
+            else
+            {
+                //убираем пробелы, включаем однострочный режим, ищем цифру
+                if (!Regex.IsMatch(login.Trim(), @"^[0-9]", RegexOptions.Singleline))
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        public bool passwordMatching(string password)
+        {
+            bool result = true;
+
+            if (password.Length < 8 || password.Length > 16)
+            {
+                result = false;
+            }
+            else
+            {
+                //убираем пробелы, включаем однострочный режим, ищем минимум одну заглавную букву, одну строчную букву и одну цифру
+                if (!(Regex.IsMatch(password.Trim(), @"[0-9]", RegexOptions.Singleline) 
+                    && Regex.IsMatch(password.Trim(), @"[a-z]", RegexOptions.Singleline)
+                    && Regex.IsMatch(password.Trim(), @"[A-Z]", RegexOptions.Singleline)))
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
 
         public bool Reg(string login, string password)
         {
@@ -42,6 +98,7 @@ namespace Contacts.Services.Authorization
             
             return result;
         }
+
         public bool Login(string login, string password)
         {
             _status = false;
@@ -58,6 +115,24 @@ namespace Contacts.Services.Authorization
             }
 
             return _status;
+        }
+        public async Task<bool> ChangePassword(string newPassword)
+        {
+            string tmp = _profile.Password;
+
+            _profile.Password = newPassword;
+            int res = await _repository.UpdateAsync(_profile);
+
+            if (res > 0)
+            {
+                return true;
+            }
+            else
+            {
+                //в случаи неудачи возврат старого пароля
+                _profile.Password = tmp;
+                return false;
+            }
         }
 
         public void LogOut()
--- Contacts/Contacts/Services/Authorization/IAuthorization.cs
+++ Contacts/Contacts/Services/Authorization/IAuthorization.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Contacts.Services.Authorization
 {
@@ -10,8 +11,12 @@ namespace Contacts.Services.Authorization
         bool Status { get; }
         User Profile { get;}
 
+        bool CheckForUse(string login);
+        bool loginMatching(string login);
+        bool passwordMatching(string password);
         bool Reg(string login, string password);
         bool Login(string login, string password);
+        Task<bool> ChangePassword(string newPassword);
         void LogOut();
     }
 }
